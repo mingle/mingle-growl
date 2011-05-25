97471dce07cd756c1ef5eaee6f0a9022d908d331
Peter Gillard-Moss <peter.moss@gmail.com>
2011-05-25 13:53:15 +0100

    Spike stubbing mingle event feed

--- spec/scenario_spec.rb
+++ spec/scenario_spec.rb
@@ -7,8 +7,7 @@ Spec::Runner.configure do |conf|
   conf.mock_with :rr
 end
 
-# describe GNTP behavior
-describe GNTP do
+describe "spike growl stubbing" do
   include GNTPExampleHelperMethods
 
   before do
@@ -17,7 +16,7 @@ describe GNTP do
     @opened_socket = create_stub_socket(@ok_response, @sended_messages)
   end
 
-  it "can register notification with minimum params" do
+  it "can stub out GNTP socket" do
     APP_NAME = 'Mingle Growl'
     TITLE = 'Fake title'
     TEXT = 'Fake text'
@@ -36,4 +35,56 @@ describe GNTP do
       @sended_messages.last.should include(expected_text)
     }
   end
+end
+
+describe "spike events stubbing" do
+  require 'mingle_events'
+  require 'fakeweb'
+  require 'fakefs/spec_helpers'
+  include FakeFS::SpecHelpers
+
+  before do
+    FakeWeb.register_uri(:get, "https://user:password@mingle.events.com/api/v2/projects/project1/feeds/events.xml", :body => events)
+  end
+
+  it "picks up events" do
+
+    mingle_access = MingleEvents::MingleBasicAuthAccess.new(
+                                                            'https://mingle.events.com',
+                                                            'user',
+                                                            'password'
+                                                            )
+
+    state_folder = File.dirname('bookmark')
+
+    stub_publisher = StubPublisher.new
+    processors = MingleEvents::Processors::Pipeline.new( [ stub_publisher ])
+
+    processors_by_project = {
+      'project1' => [processors]
+    }
+
+    MingleEvents::Poller.new(mingle_access, processors_by_project, state_folder).run_once
+
+    stub_publisher.entries.should include 'http://mingle.events.com/projects/project1/events/index/413027'
+  end
+end
+
+class StubPublisher < MingleEvents::Processors::AbstractNoRetryProcessor
+  def process_event(entry)
+    entries << entry.entry_id
+  end
+
+  def entries() @entries ||= [] end
+end
+
+def events
+  <<EVENTS
+<?xml version="1.0" encoding="UTF-8"?>
+<feed xmlns="http://www.w3.org/2005/Atom" xmlns:mingle="http://www.thoughtworks-studios.com/ns/mingle">
+  <entry>
+    <id>http://mingle.events.com/projects/project1/events/index/413027</id>
+  </entry>
+</feed>
+EVENTS
 end
