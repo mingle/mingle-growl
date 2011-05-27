82e065ff4aff9481480e712b47e3b419847d82e3
Peter Gillard-Moss <peter.moss@gmail.com>
2011-05-27 12:14:03 +0100

    Adding message matchers

--- spec/scenario_spec.rb
+++ spec/scenario_spec.rb
@@ -23,15 +23,14 @@ describe "scenarios" do
   end
 
   it "growls the event from mingle" do
-    title = 'Story #26 As a user I want this to work'
     text = 'Something changed'
 
     mingle_growl.growl
 
     @received_messages.last.should have_application_name 'Mingle Growl'
+    @received_messages.last.should have_title 'Story #26 As a user I want this to work'
 
     [
-     "Notification-Title: #{title}\r\n",
      "Notification-Text: #{text}\r\n"
     ].each {|expected_text|
       @received_messages.last.should include(expected_text)
@@ -58,6 +57,18 @@ Spec::Matchers.define :have_application_name do |expected_application_name|
 
   failure_message_for_should do |message|
     "expected GNTP message to have 'Application-Name: #{expected_application_name}' but was '#{@application_name}'"
+  end
+end
+
+Spec::Matchers.define :have_title do |expected_title|
+  match do |message|
+    @title = message.select {|l| l.include? 'Notification-Title' }.first || ''
+    @title = @title.strip
+    @title == "Notification-Title: #{expected_title}"
+  end
+
+  failure_message_for_should do |message|
+    "expected GNTP message to have 'Notification-Title: #{expected_title}' but was '#{@title}'"
   end
 end
 
